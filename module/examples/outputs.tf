77b6706f45747be229a855ec7e14eb3fef55b1f0
Kevin Contreras <kvncont@gmail.com>
2023-12-13 21:16:58 -0600

    fix: update module outputs.tf to use container_apps_solution

--- module/examples/outputs.tf
+++ module/examples/outputs.tf
@@ -1,11 +1,11 @@
 output "kv_endpoint" {
-  value = module.my_solution.kv_endpoint
+  value = module.container_apps_solution.kv_endpoint
 }
 
 output "managed_identity_object_id" {
-  value = module.my_solution.managed_identity_object_id
+  value = module.container_apps_solution.managed_identity_object_id
 }
 
 output "container_apps_endpoint" {
-  value = "https://${module.my_solution.container_apps_endpoint}"
+  value = "https://${module.container_apps_solution.container_apps_endpoint}"
 }
